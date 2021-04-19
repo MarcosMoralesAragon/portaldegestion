71d67ec2bb3b4be210d63829a96232c10db8e925
Marcos <marcosmoralesaragon@gmail.com>
2021-04-19 10:29:19 +0200

    Prueba

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,7 @@ public class Main {
             System.out.println("3. Borrar");
             System.out.println("4. Modificar");
             System.out.println("5. Salir");
+            System.out.println("5. Salir");
             Prints.separador();
             System.out.print(" > ");
 
